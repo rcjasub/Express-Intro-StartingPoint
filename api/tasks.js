1ebf642e7a52fcf1791ea331b53c47e0a4aaf6c0
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-26 15:54:37 -0400

    tasks functions

--- api/tasks.js
+++ api/tasks.js
@@ -2,33 +2,55 @@ const express = require("express");
 const router = express.Router();
 const { Task } = require("../dummy-database");
 
-router.get("/", (req, res) => {
- const response = Task.findByPk(1);
- res.send(response);
-});
+
+sampleTask = {
+    id: 4,
+    title: "Test",
+    description: "Test",
+    completed: false,
+    userId: 2,
+};
 
 // GET all tasks
 router.get("/", (req, res) => {
-  // Replace this with your code!
-   res.send(Task.findAll());
+  const task = Task.findAll();
+  res.json(task);
 });
 
-
-
-
-const app = express
-
-// GET a single task by id
 
 // Patch a task by id
-
-router.get("/", (req, res) => {
- res.send(Task.update(2, Task));
+router.patch("/", (req, res) => {
+  const task = Task.update(2, sampleTask);
+  res.send(task);
 });
+
+
+
+// GET a single task by id
+/*
+router.get("/", (req, res) => {
+  const task = Task.findByPk(2);
+  res.status(200).send(task)
+});
+*/
+
+
+
 
 
 // Delete a task by id
+router.delete("/", (req, res) => {
+  const task = Task.delete(req.body);
+  res.send(task);
+});
+
 
 // Create a new task
+
+router.post("/", (req, res) => {
+
+  const task = Task.create(req.body);
+  res.send(task);
+});
 
 module.exports = router;
